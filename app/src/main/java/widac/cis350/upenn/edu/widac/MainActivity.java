c0a7fdcc87f6b690563a33e31956b0ce2c625cc3
matthewliang <matthewl97@yahoo.com>
2017-02-16 19:10:27 -0500

    Wire frame first screens

---
+++ app/src/main/java/widac/cis350/upenn/edu/widac/MainActivity.java
@@ -0,0 +1,31 @@
+package widac.cis350.upenn.edu.widac;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onSearchButtonClick(View v) {
+        Intent i = new Intent(this, SearchActivity.class);
+        startActivityForResult(i, 1);
+    }
+
+    public void onSettingsButtonClick(View v) {
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivityForResult(i, 1);
+    }
+}
