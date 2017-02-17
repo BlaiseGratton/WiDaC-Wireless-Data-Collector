2134daa4c6519459f9fd98ce218f3aff55f9faaf
matthewliang <matthewl97@yahoo.com>
2017-02-16 21:53:56 -0500

    Added visualization activity

---
+++ app/src/main/java/widac/cis350/upenn/edu/widac/VisualizationActivity.java
@@ -0,0 +1,13 @@
+package widac.cis350.upenn.edu.widac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VisualizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualization);
+    }
+}
