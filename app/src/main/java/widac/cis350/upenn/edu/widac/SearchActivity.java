c86da0aeeb8e026c21c151e08bf5183992a42b51
Nicolo` Marzaro <Nicolo@may192.wireless-resnet.upenn.edu>
2017-02-23 23:48:13 -0500

    passed in searhed key to SearchActivity

--- app/src/main/java/widac/cis350/upenn/edu/widac/SearchActivity.java
+++ app/src/main/java/widac/cis350/upenn/edu/widac/SearchActivity.java
@@ -1,29 +1,34 @@
 package widac.cis350.upenn.edu.widac;
-
+import widac.cis350.upenn.edu.widac.models.Sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Intent;
 
 import static java.security.AccessController.getContext;
 
 public class SearchActivity extends AppCompatActivity {
-
+    
+    private Sample sample;
+    DBConnection db;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-
+        db = new DBConnection();
+        Intent i = this.getIntent();
+        sample  = db.retrieveSample(i.getStringExtra("compositeKey"));
     }
-
+    
     public void onUpdateBluetoothButtonClick(View v) {
         ((TextView)findViewById(R.id.itemWeight)).setText("Weight: 234g");
         Toast.makeText(this, "Weight updated", Toast.LENGTH_LONG).show();
     }
-
+    
     public void onUpdateManualButtonClick(View v) {
         EditText editText = (EditText) findViewById(R.id.new_weight);
         String newWeight = editText.getText().toString();
