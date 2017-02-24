93ba0ab5d1daca9c10270e541bd0bb41ae2979e8
Ashutosh Agrawal <ashutosha.56@gmail.com>
2017-02-24 17:29:28 -0500

    Refactored package structure for retrofit

--- app/src/main/java/widac/cis350/upenn/edu/widac/data/remote/WidacService.java
+++ app/src/main/java/widac/cis350/upenn/edu/widac/data/remote/WidacService.java
@@ -1,8 +1,17 @@
 package widac.cis350.upenn.edu.widac.data.remote;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import widac.cis350.upenn.edu.widac.models.Sample;
+
 /**
  * Created by ashutosh on 2/24/17.
  */
 
-public class WidacService {
+public interface WidacService {
+
+    @GET("/widac/api/v1.0/samples/{composite_key}")
+    Call<Sample> retrieveSample(@Path("composite_key") String composite_key);
 }
