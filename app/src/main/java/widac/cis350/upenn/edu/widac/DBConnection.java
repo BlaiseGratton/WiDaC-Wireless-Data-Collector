e8e108155f24d1f13f32e896b0852d35adaa09b7
Ashutosh Agrawal <ashutosha.56@gmail.com>
2017-02-23 20:48:26 -0500

    Initial DBConnection interface

---
+++ app/src/main/java/widac/cis350/upenn/edu/widac/DBConnection.java
@@ -0,0 +1,33 @@
+package widac.cis350.upenn.edu.widac;
+
+import widac.cis350.upenn.edu.widac.models.Sample;
+
+/**
+ * Created by ashutosh on 2/23/17.
+ */
+
+public class DBConnection {
+
+    public DBConnection() {
+
+    }
+
+    public void addNewSample(Sample newSample) {
+//        Sample newSample = new Sample(344, 120, 1, 2, "ceramic", 0.002);
+    }
+
+    public void updateSampleMaterial(String compositeKey, String newMaterial) {
+//        String compositeKey = "344-120-1-2";
+//        String newMaterial = "ceramic";
+    }
+
+    public void updateSampleWeight(String compositeKey, double newWeight) {
+//        String compositeKey = "344-120-1-2";
+//        double newWeight = 0.002;
+    }
+
+    public Sample retrieveSample(String compositeKey) {
+            return new Sample(344, 120, 1, 2, "ceramic", 0.002);
+    }
+
+}
