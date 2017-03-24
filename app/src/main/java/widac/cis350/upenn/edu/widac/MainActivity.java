9cd3e560ec576fd523d7094a5caed12d67ae5d7d
matthewliang <matthewl97@yahoo.com>
2017-03-24 16:46:18 -0400

    Bluetooth first attempt

--- app/src/main/java/widac/cis350/upenn/edu/widac/MainActivity.java
+++ app/src/main/java/widac/cis350/upenn/edu/widac/MainActivity.java
@@ -1,5 +1,7 @@
 package widac.cis350.upenn.edu.widac;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,16 +9,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     
     public final static String COMPOSITE_KEY = "compositeKey";
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Session.newSession();
+
+        /*
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }*/
+        MyBluetoothService bluetoothService = new MyBluetoothService();
     }
     
     public void onSearchButtonClick(View v) {
