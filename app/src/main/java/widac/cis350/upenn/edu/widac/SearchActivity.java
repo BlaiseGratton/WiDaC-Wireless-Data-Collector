4e4fd4df0f3c11fd8cd62084932ef85ccdb29eb3
Nicolo` Marzaro <Nicolo@may417.wireless-resnet.upenn.edu>
2017-03-29 18:46:11 -0400

    Changed UI in Search Activity: item id now shows between prev and next button, manual button is smaller, manual entry field only pops up and request update upon clicking the button

--- app/src/main/java/widac/cis350/upenn/edu/widac/SearchActivity.java
+++ app/src/main/java/widac/cis350/upenn/edu/widac/SearchActivity.java
@@ -11,6 +11,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 import static java.security.AccessController.getContext;
 
@@ -19,32 +21,24 @@ public class SearchActivity extends AppCompatActivity {
     private Sample sample;
     DBConnection db;
     private int itemNumber;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-        // Create new intent
+        db = new DBConnection();
         Intent i = this.getIntent();
 
-        // Pull from database
-        // sample = Session.pullNewEntryFromDB(i.getStringExtra(("compositeKey")));
-        // sample  = db.retrieveSample(i.getStringExtra("compositeKey"));
 
-        // Update
-        /*
         sample = db.retrieveSample(i.getStringExtra("compositeKey"));
->>>>>>> 6be840555b12c52ee78bcfc5e3d82e01b45078e6
         TextView itemName = (TextView) findViewById(R.id.item_name);
+        sample  = db.retrieveSample(i.getStringExtra("compositeKey"));
         itemName.setText(sample.getCompositeKey());
         TextView itemWeight = (TextView) findViewById(R.id.itemWeight);
         String displayWeight = (sample.getWeight() == 0) ? "No Data" : "" + sample.getWeight();
         itemWeight.setText(displayWeight);
-        */
-        //db = new DBConnection();
-        //db.getSample(i.getStringExtra("id"), sampleCallback);
-        Session.asyncPullNewEntry(i.getStringExtra("id"), sampleCallback);
+
+        db.getSample(i.getStringExtra("id"), sampleCallback);
 
         itemNumber = 123;
     }
@@ -55,10 +49,34 @@ public class SearchActivity extends AppCompatActivity {
     }
     
     public void onUpdateManualButtonClick(View v) {
-        EditText editText = (EditText) findViewById(R.id.new_weight);
-        String newWeight = editText.getText().toString();
-        ((TextView)findViewById(R.id.itemWeight)).setText("Weight: " + newWeight + "g");
-        Toast.makeText(this, "Weight updated", Toast.LENGTH_LONG).show();
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Manual Update");
+        alert.setMessage("Input weight in grams:");
+
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String newWeight = input.getText().toString();
+                try {
+                    Double.parseDouble(newWeight);
+                    ((TextView)findViewById(R.id.itemWeight)).setText("Weight: " + newWeight + "g");
+                } catch (Exception e) {
+                    Toast.makeText(SearchActivity.this,
+                            "Input must be a number.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
     }
 
     public void onPrevButtonClick(View v) {
@@ -83,23 +101,22 @@ public class SearchActivity extends AppCompatActivity {
         public void onResponse(Call<Sample> call, Response<Sample> response) {
             int code = response.code();
             if (code == 200) {
-                Log.d("SearchActivity", "CALLING SAMPLECALLBACK SEARCH ACTIVITY");
                 SearchActivity.this.sample = response.body();
                 TextView itemName = (TextView) findViewById(R.id.item_name);
                 itemName.setText(sample.getCompositeKey());
                 TextView itemWeight = (TextView) findViewById(R.id.itemWeight);
                 String displayWeight = (sample.getWeight() == 0) ? "No Data" : "" + sample.getWeight();
                 itemWeight.setText("Weight: " + displayWeight + "g");
-                Log.d("SearchActivity", "Got the sample: " + sample.toString() + " Material: " + sample.getMaterial());
+                Log.d("DBConnection", "Got the sample: " + sample.toString() + " Material: " + sample.getMaterial());
             } else {
-                // Bad style for now,
-                Log.d("SearchActivity", "Did not work: " + String.valueOf(code));
+                Log.d("DBConnection", "Did not work: " + String.valueOf(code));
             }
         }
 
         @Override
         public void onFailure(Call<Sample> call, Throwable t) {
-            Log.d("SearchActivity", "Get sample failure");
+            Log.d("DBConnection", "Get sample failure");
         }
     };
+
 }
